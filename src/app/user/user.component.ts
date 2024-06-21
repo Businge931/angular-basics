fadd24fe919b7478650fec343b3d3d68a3cf5ee2
Businge931 <jimmybusinge9@gmail.com>
2024-06-21 12:19:48 +0300

    40 - TypeScript Type Aliases Interfaces

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,11 +14,7 @@ import { Component, Input, Output, EventEmitter, output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
